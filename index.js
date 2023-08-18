bf42441bdd2adcdd0d40e6118eca9991fbd82c7b
Anjali Kulkarni <anjali.kulkarni@mit.asia>
2023-08-18 10:32:18 +0530

    using bootstrap

---
+++ index.js
@@ -0,0 +1,40 @@
+// var count = 0;
+  
+// const totalValue = document.getElementById('total-count');
+// const incrementBtn = document.getElementById('bt4');
+// const decrementBtn = document.getElementById('bt5');
+// totalValue.innerHTML = count;
+// bt5.addEventListener('click', () => {
+//     count++;
+//     totalValue.innerHTML = count;
+// });
+  
+// bt4.addEventListener('click', () => {
+//     count--;
+//     totalValue.innerHTML = count;
+// });
+// const numberElement = document.getElementById('total_count');
+// const plusButton = document.getElementById('bt5');
+// const minusButton = document.getElementById('bt4');
+
+// let currentNumber = 0;
+
+// // Function to update the number display
+// function updateNumberDisplay() {
+//     numberElement.textContent = currentNumber;
+// }
+
+// // Event listener for the plus button
+// plusButton.addEventListener('click', () => {
+//     currentNumber += 1;
+//     updateNumberDisplay();
+// });
+
+// // Event listener for the minus button
+// minusButton.addEventListener('click', () => {
+//     currentNumber -= 1;
+//     updateNumberDisplay();
+// });
+
+// // Initial display update
+// updateNumberDisplay();
